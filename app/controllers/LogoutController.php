e1fa21bacb7cff908161d79e683e5e46e1483b66
Quuaann <rjck024vipro@gmail.com>
2025-05-26 23:59:47 +0700

    login google

--- app/controllers/LogoutController.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Xóa tất cả biến session
-session_destroy(); // Hủy session
-
-// Chuyển hướng về trang đăng nhập
-header('Location: /index.php');
-exit();
