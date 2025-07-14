839331bfa3c411a06eba46b358fbef246aa4dbda
Quuaann <rjck024vipro@gmail.com>
2025-07-14 13:34:00 +0700

    update

--- controllers/lesson/learn.php
+++ controllers/lesson/learn.php
@@ -1,6 +1,10 @@
 <?php
 $app->router("/learn", 'GET', function($vars) use ($app) {
-    
+    if(!$app->getSession("accounts")){
+        $vars['templates'] = 'login';
+        echo $app->render('templates/login.html', $vars);
+        return;
+    }
     echo $app->render('templates/lessons/learn.html',$vars);
 });
 
